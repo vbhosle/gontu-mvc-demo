1f2ab741074513db8eb338849274557a373acc8c
Viraj Bhosle <bhosleviraj@gmail.com>
2018-10-10 19:59:11 +0530

    Spring MVC Tutorials 20 - Form Validations 04 (@Pattern, @Past, @Max and some more...)

--- src/main/java/com/gontuseries/model/Student.java
+++ src/main/java/com/gontuseries/model/Student.java
@@ -3,16 +3,23 @@ package com.gontuseries.model;
 import java.util.ArrayList;
 import java.util.Date;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 public class Student {
-
+	@Pattern(regexp="[^0-9]*")
 	private String studentName;
 	
 //	@Size(min=2, max=30, message="hobby must be described between {min} to {max} words")
 	@Size(min=2, max=30) //error message specified in properties file
 	private String studentHobby;
+	
+	@Max(2222)
 	private Long studentMobile;
+	
+	@Past
 	private Date studentDOB;
 	private ArrayList<String> studentSkills;
 	private Address studentAddress;
